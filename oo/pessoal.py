2720d2cdbe1c7d35c33c147e6aa6565306ae849a
rsilva <ricardosilvablv@gmail.com>
2021-03-10 23:53:21 -0300

    Criados atributos de instâncias nome

--- oo/pessoal.py
+++ oo/pessoal.py
@@ -1,7 +1,11 @@
 class Pessoa:
+    def __init__(self):
+        self.nome = None
+
     def cumprimentar(self):
         return 'Olá'
 
 if __name__ == '__main__':
     p = Pessoa()
     print(Pessoa.cumprimentar(p))
+    print(p.nome)
